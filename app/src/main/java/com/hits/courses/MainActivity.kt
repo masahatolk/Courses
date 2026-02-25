6c542e7f836fef93f759c3146d6b0914cc33f052
Tolkacheva Maria <141151654+masahatolk@users.noreply.github.com>
2026-02-25 12:00:42 +0700

    account верстка шаблона

--- app/src/main/java/com/hits/courses/MainActivity.kt
+++ app/src/main/java/com/hits/courses/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hits.courses
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.hits.feature.account.ui.AccountFragment
 import com.hits.feature.main.databinding.ActivityMainBinding
 import com.hits.feature.main.ui.MainFragment
 
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
             val selectedFragment = when(item.itemId) {
                 com.hits.feature.main.R.id.nav_main -> MainFragment()
                 //com.hits.feature.main.R.id.nav_favorites -> FavoritesFragment()
-                //com.hits.feature.main.R.id.nav_account -> AccountFragment()
+                com.hits.feature.main.R.id.nav_account -> AccountFragment()
                 else -> MainFragment()
             }
 
---
+++ feature-account/src/main/java/com/hits/feature/account/ui/AccountFragment.kt
@@ -0,0 +1,7 @@
+package com.hits.feature.account.ui
+
+import androidx.fragment.app.Fragment
+import com.hits.feature.account.R
+
+class AccountFragment : Fragment(R.layout.fragment_account) {
+}
